896a5dbf26f6d223985e15da5d6313b63499b66e
NehuenCuenca <nehuen.cuenca@hotmail.com>
2020-07-13 15:17:24 -0300

    Commit de Eloquent y ejercicios JS

---
+++ database/migrations/2020_06_16_195556_create_perfiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfiles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('facebook');
+            $table->string('instagram');
+            $table->string('twitter');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfiles');
+    }
+}
